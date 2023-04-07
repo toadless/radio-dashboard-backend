9dd2e74ee4f85fa2b97d1948131e4924a4e29b3f
toadless <me@toadless.net>
2023-04-07 21:17:42 +0100

    web module

---
+++ src/main/java/net/toadless/radio/modules/WebModule.java
@@ -0,0 +1,58 @@
+package net.toadless.radio.modules;
+
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+import io.javalin.http.Context;
+import io.javalin.http.Header;
+import io.javalin.plugin.bundled.CorsPluginConfig;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import net.toadless.radio.Radio;
+import net.toadless.radio.objects.config.ConfigOption;
+import net.toadless.radio.objects.module.Module;
+import net.toadless.radio.objects.module.Modules;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class WebModule extends Module
+{
+    private final Javalin javalin;
+
+    public WebModule(Radio radio, Modules modules)
+    {
+        super(radio, modules);
+        this.javalin = Javalin
+                .create(this::setJavalinConfig)
+                .routes(() ->
+                {
+                    path("/health", () -> get(ctx -> ctx.result("Healthy")));
+                }).start(radio.getConfiguration().getInt(ConfigOption.PORT));
+    }
+
+    public Radio getRadio()
+    {
+        return radio;
+    }
+
+    public Javalin getJavalin()
+    {
+        return javalin;
+    }
+
+    public void ok(Context context, DataObject payload)
+    {
+        result(context, 200, payload);
+    }
+
+    public void result(Context ctx, int code, DataObject data)
+    {
+        ctx.header(Header.CONTENT_TYPE, "application/json");
+        ctx.status(code);
+        ctx.result(data.toString());
+    }
+
+    public void setJavalinConfig(JavalinConfig config)
+    {
+        config.showJavalinBanner = false;
+        config.plugins.enableCors(cors -> cors.add(CorsPluginConfig::anyHost));
+    }
+}
